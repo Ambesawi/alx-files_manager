5896b8d6e8b5bd6f2a54b38dc4911c9e6769314d
Ambesawi <ambesawi77@gmail.com>
2023-12-05 10:57:17 -0800

    AppController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
