5319d1976aea6b47489bdfd205cf7e804fcd43b1
Ambesawi <ambesawi77@gmail.com>
2023-12-05 10:48:19 -0800

    main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+import redisClient from './utils/redis';
+
+(async () => {
+    console.log(redisClient.isAlive());
+    console.log(await redisClient.get('myKey'));
+    await redisClient.set('myKey', 12, 5);
+    console.log(await redisClient.get('myKey'));
+
+    setTimeout(async () => {
+        console.log(await redisClient.get('myKey'));
+    }, 1000*10)
+})();
