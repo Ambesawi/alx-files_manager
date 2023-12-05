b08ddcbbea48865a6a6c072fd7aecb3fc4e3c02a
Ambesawi <ambesawi77@gmail.com>
2023-12-05 10:58:19 -0800

    AuthController.js

---
+++ controllers/AuthController.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-named-as-default */
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
+    const token = uuidv4();
+
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
+  }
+}
