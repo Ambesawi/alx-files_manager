5b64fe7d43563a97c1a70644e32ade1cc8bc6956
Ambesawi <ambesawi77@gmail.com>
2023-12-05 10:51:54 -0800

    env_loader.js

---
+++ utils/env_loader.js
@@ -0,0 +1,22 @@
+import { existsSync, readFileSync } from 'fs';
+
+/**
+ * Loads the appropriate environment variables for an event.
+ */
+const envLoader = () => {
+  const env = process.env.npm_lifecycle_event || 'dev';
+  const path = env.includes('test') || env.includes('cover') ? '.env.test' : '.env';
+
+  if (existsSync(path)) {
+    const data = readFileSync(path, 'utf-8').trim().split('\n');
+
+    for (const line of data) {
+      const delimPosition = line.indexOf('=');
+      const variable = line.substring(0, delimPosition);
+      const value = line.substring(delimPosition + 1);
+      process.env[variable] = value;
+    }
+  }
+};
+
+export default envLoader;
